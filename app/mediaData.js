d5350333fd062f997ad64b3eb85e84f7bffba2b9
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-04 14:39:16 -0300

    Monta ativos visuais da apostila

---
+++ app/mediaData.js
@@ -0,0 +1,9 @@
+import cover1 from './media/chunks/cover_1';
+import cover2 from './media/chunks/cover_2';
+import cover3 from './media/chunks/cover_3';
+import cover4 from './media/chunks/cover_4';
+import cover5 from './media/chunks/cover_5';
+import portrait1 from './media/chunks/portrait_1';
+
+export const apostilaCover = `data:image/webp;base64,${cover1}${cover2}${cover3}${cover4}${cover5}`;
+export const margarethPhoto = `data:image/webp;base64,${portrait1}`;
